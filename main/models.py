84dfa754844cfea3b5fac370229660b3a30b6d75
Yerassyl Tleugazy <tleugazy98@gmail.com>
2020-11-08 19:21:15 +0600

    sdsdd

--- main/models.py
+++ main/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 
-# Create your models here.
 class City(models.Model):
     name = models.CharField(max_length=255)
+    coordinateX = models.FloatField(default=0.0)
+    coordinateY = models.FloatField(default=0.0)
 
     def __str__(self):
         return self.name
--- main/views.py
+++ main/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from .models import *
+import random
 
-# Create your views here.
+
+def index(request):
+    if request.method == "POST":
+        fromCity = int(request.POST["from"])
+        toCities = request.POST.getlist("to")
+        toCities = [int(i) for i in toCities]
+        toCities = [City.objects.get(pk=i) for i in toCities]
+        fromCity = City.objects.get(pk=fromCity)
+        directories = [fromCity]
+        temp = fromCity
+        for i in toCities:
+            print(i)
+        while True:
+            if len(toCities) == 0:
+                break
+            distances = Distance.objects.filter(fromCity=temp, toCity__in=toCities).order_by("distance")
+            temp = distances[0].toCity
+            toCities.remove(temp)
+            directories.append(distances[0])
+        last = Distance.objects.get(fromCity=directories[len(directories)-1].toCity, toCity=fromCity)
+        directories.append(last)
+        print("after while")
+        for i in directories:
+            print(i)
+
+    cities = City.objects.all()
+    context = {
+        "cities": cities,
+    }
+    # test_data()
+    return render(request, "main/index.html", context=context)
+
+
+def test_data():
+    cities = City.objects.all()
+    for fromCity in cities:
+        for toCity in cities:
+            if fromCity != toCity:
+                test = Distance(fromCity=fromCity, toCity=toCity, distance=random.randint(100, 1000))
+                test.save()
